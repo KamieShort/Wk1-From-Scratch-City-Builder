46fb2a9dc5ab6aaf381cb1f6aec0e5e971e769d6
Kamie Short <kamie.short@gmail.com>
2022-01-27 17:45:14 -0800

    added city-names button and results section in index, grabbed el by Id

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // import functions and grab DOM elements
+const buildingSelector = document.getElementById('building-selector');
+const buildingImage = document.getElementById('building-img');
 
+const mayorSelector = document.getElementById('mayor-selector');
+const mayorImage = document.getElementById('mayor-img');
+
+const pestSelector = document.getElementById('pest-selector');
+const pestImage = document.getElementById('pest-img');
 // let state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
